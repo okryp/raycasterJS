e633f7d65d9914fbc4b5487af181b448f38df1a6
jrasb <okrypczyk@protonmail.com>
2023-06-12 14:24:50 +0200

    end of class commit

--- js/main.js
+++ js/main.js
@@ -1,90 +1,105 @@
 "use strict"
-console.log("---\tMAIN LOADED\t---");
 
-const HEIGHT = 600;
-const WIDTH = 800;
+const COS = Math.cos;
+const SIN = Math.sin;
+const TAN = Math.tan;
+const INT = Math.floor;
 
-// Tick update every n seconds;
-const TICKSPEED = 0.2;
+// --- DOM --- //
+const CANVAS_WIDTH = 800
+const CANVAS_HEIGHT = 600
 
-// MAP RELATED VARS
-const MAP_WIDTH = 8;
-const MAP_HEIGHT = 8;
-const map = [
-    // 8x8 map 
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 1, 0, 1,
-    1, 0, 0, 0, 1, 1, 0, 1,
-    1, 0, 0, 0, 0, 1, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1, 1
-];
-// 32x32px blocks
-const MAP_BLOCK_SIZE = 32;
+document.querySelector("#wrapper").innerHTML = `
+    <canvas id="canvas" width="${CANVAS_WIDTH}" height="${CANVAS_HEIGHT}"></canvas> 
+`
 
-// PLAYER OBJECT THAT STORES THE IMPORTANT DATA
-const PLAYER = {
-    pos_x: 0,
-    pos_y: 0,
-    // deg
-    rotation: 0,
-    movement_speed: 0.1,
-    overflow_rotation: () => {
-        if (rotation = 360) rotation = 0;
-    }
-};
-
-const wrapper= document.querySelector("div#wrapper");
-
-const createCanvas = (width, height) => {
-    if (!wrapper) return 1;
-
-    wrapper.innerHTML = `
-    <canvas id="canvas" width="${width}" height="${height}"></canvas>
-    `
-}
-
-createCanvas(WIDTH, HEIGHT);
-
-const drawPlayer = (ctx) => {
-    ctx.fillStyle = "yellow"
-    ctx.fillRect(PLAYER.pos_x-0.5, PLAYER.pos_y-0.5, 1, 1);
-    overflow_rotation
-}
-
-// Canvas Vars
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#666"
-ctx.fillRect(0, 0, WIDTH, HEIGHT);
+// --- MAP --- //
+const MAP_WIDTH = 8;
+const MAP_HEIGHT = 8;
+// W x H
+const MAP_SIZE = 64;
 
-const draw = () => {
-    // TODO
-    drawPlayer(ctx);
-    console.log(`player x: ${PLAYER.pos_x}\nplayer y: ${PLAYER.pos_y}`);
+const MAP = [
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 1, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+];
+
+// --- PLAYER --- //
+
+const degToRad = (angle) => angle*Math.PI/180;
+const fixAngle = (angle) => {
+    if (angle > 359) {angle -= 360} 
+    if (angle < 0) {angle += 360}
 }
 
-// setInterval(draw, 0.2); 
+let px = 0, py = 0, pdx = 0, pdy = 0, pa = 0;
 
 window.addEventListener("keydown", e => {
-    switch(e.key) {
-        case "w":
-            PLAYER.pos_y -= PLAYER.movement_speed;
-            break;
-        case "d":
-            PLAYER.pos_x += PLAYER.movement_speed;
-            break;
-        case "s":
-            PLAYER.pos_y += PLAYER.movement_speed;
-            break;
-        case "a":
-            PLAYER.pos_x -= PLAYER.movement_speed;
-    }  
-    if (PLAYER.pos_x < 0) PLAYER.pos_x = 0;
-    if (PLAYER.pos_y < 0) PLAYER.pos_y = 0;
-    if (PLAYER.pos_x > WIDTH) PLAYER.pos_x = WIDTH;
-    if (PLAYER.pos_y > HEIGHT) PLAYER.pos_y = HEIGHT;
+    if (e.key == "a") { pa += 5; pa = fixAngle(pa); pdx = COS(degToRad(pa)); pdy = SIN(degToRad(pa));}
+    if (e.key == "a") { pa -= 5; pa = fixAngle(pa); pdx = COS(degToRad(pa)); pdy = SIN(degToRad(pa));}
+    if (e.key == "w") { px += pdx * 5; py += pdy * 5}
+    if (e.key == "s") { px -= pdx * 5; py -= pdy * 5}
 });
+
+// --- RAYCASTING --- //
+
+const distance = (ax, ay, bx, by, ang) => {return COS(degToRad(ang))*(bx-ax)-SIN(degToRad(ang))*(by-ay)};
+
+const drawRays = (ctx) => {
+    // Draw ground
+    ctx.fillStyle = "#0ff";
+    ctx.fillRect(0, 0 , CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillStyle = "#00f";
+    ctx.fillRect(0, CANVAS_HEIGHT/2, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    let r, mx, my, mp, dof, side;
+    let vx, vy, rx, ry, ra, xo, yo, disV, disH;
+
+    ra = fixAngle(pa+30);
+
+    for (r = 0; r < 60; r++) {
+        // vertical
+        dof = 0; side = 0; disV = 0;
+        let tan = TAN(degToRad(ra));
+        if (COS(degToRad(ra)) > 0.001) {
+            rx = ((INT(px)>>6)<<6) + 64;
+            ry = (px - rx) * tan + py;
+            xo = 64; yo = -xo * tan;
+        } else if (COS(degToRad(ra)) < -0.001) {
+            rx = ((INT(px)>>6)<<6) - 0.0001;
+            ry = (px - rx) * tan + py;
+            xo = -64; yo = -xo * tan;
+        } else {
+            rx = px; ry = py; dof = 8;
+        }
+        vx=rx; vy=ry;
+        // horizontal
+        dof = 0; disH=100000;
+        tan = 1.0/tan;
+        if (SIN(degToRad(ra)) > 0.001) { 
+            ry = ((INT(py)>>6)<<6) - 0.0001;
+            rx = (py - ry) * tan + px;
+            yo = -64; xo = -yo * tan; 
+        } else if (SIN(degToRad(ra)) < -0.001) {
+            ry = ((INT(py)>>6)<<6) + 64;
+            rx = (py - ry) * tan + px;
+            yo = 64; xo = -yo * tan;
+        } else {
+            rx = px; ry = py; dof = 8;
+        }
+
+        while (dof < 8) {
+            mx = INT(rx) >> 6; my = INT(ry) >> 6; mp = my * MAP_WIDTH + mx;
+             
+        }
+    }
+}
